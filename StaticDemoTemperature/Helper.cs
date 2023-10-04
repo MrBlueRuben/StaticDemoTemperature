a077ef929f4e8c57ccf4ed06475a0074ab725078
Ruben Mrblue <sanchez.alvarez.ruben@gmail.com>
2023-10-04 09:04:39 +0200

    added helper

---
+++ StaticDemoTemperature/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemoTemperature
+{
+    public class Helper
+    {
+        public static double CelsiusToFahrenheit(string tempCelsius)
+        {
+            double celsius = 0;
+
+        }
+    }
+}
