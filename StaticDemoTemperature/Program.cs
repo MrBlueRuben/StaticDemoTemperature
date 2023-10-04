1d73ac68a955ee311224799a6dd3a63ceccfcec9
Ruben Mrblue <sanchez.alvarez.ruben@gmail.com>
2023-10-04 08:50:26 +0200

    Add project files.

---
+++ StaticDemoTemperature/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StaticDemoTemperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter C)elsius to Farenheit or\nF)arenheit to Celsius or\nK)elvin to Celsius or\nQ)uit");
+
+        }
+    }
+}
